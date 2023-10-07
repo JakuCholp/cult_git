4d2857acf5069657490424eac6e6b20069e59e01
JakuCholp <atazhanch@gmail.com>
2023-10-07 17:14:44 +0600

    kkk

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Event, Favorite, Comment, Event_Category, Reviews
+from .models import Event, Favorite, Comment, Event_Category
 
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Favorite)
-admin.site.register(Reviews)
 admin.site.register(Comment)
 admin.site.register(Event_Category)
