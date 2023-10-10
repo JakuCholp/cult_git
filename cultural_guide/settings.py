ad06391cd76d36ca56c5ee1616e4f5256dd27dd0
JakuCholp <atazhanch@gmail.com>
2023-10-10 15:19:37 +0600

    sdfsdf

--- cultural_guide/settings.py
+++ cultural_guide/settings.py
@@ -126,6 +126,7 @@ SPECTACULAR_SETTINGS = {
         'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
         'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
         'REDOC_DIST': 'SIDECAR',
+        'SEARCH_PARAM': 'search_fields',
         
     }
 
